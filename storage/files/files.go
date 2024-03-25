d7faa543d775ea6876f2befa1d96c539c6613739
Averkin Mathew <meazerg@gmail.com>
2024-03-26 00:24:36 +0300

    Storage

---
+++ storage/files/files.go
@@ -0,0 +1,138 @@
+package files
+
+import (
+	"TelegramGOBot/lib/e"
+	"TelegramGOBot/storage"
+	"encoding/gob"
+	"errors"
+	"fmt"
+	"math/rand"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+type Storage struct {
+	basePath string
+}
+
+const defaultPerm = 0774
+
+var ErrNoSavedPages = errors.New("No saved page")
+
+func New(basePath string) Storage {
+	return Storage{basePath: basePath}
+}
+
+func (s Storage) Save(page *storage.Page) (err error) {
+	defer func() { err = e.WrapIfErr("Cat't do request", err) }()
+	filePath := filepath.Join(s.basePath, page.UserName)
+
+	if err := os.MkdirAll(filePath, defaultPerm); err != nil {
+		return err
+	}
+
+	fName, err := fileName(page)
+	if err != nil {
+		return err
+	}
+
+	filePath = filepath.Join(filePath, fName)
+
+	file, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+
+		}
+	}(file)
+
+	if err := gob.NewEncoder(file).Encode(page); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s Storage) PickRandom(userName string) (page *storage.Page, err error) {
+	defer func() { err = e.WrapIfErr("Cat't do request", err) }()
+
+	path := filepath.Join(s.basePath, userName)
+
+	files, err := os.ReadDir(path)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(files) == 0 {
+		return nil, ErrNoSavedPages
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	n := rand.Intn(len(files))
+
+	file := files[n]
+
+	return s.decodePage(filepath.Join(path, file.Name()))
+}
+
+func (s Storage) Remove(p *storage.Page) error {
+	fileName, err := fileName(p)
+	if err != nil {
+		return e.Wrap("Can't remove file", err)
+	}
+
+	path := filepath.Join(s.basePath, p.UserName, fileName)
+	if err := os.Remove(path); err != nil {
+		return e.Wrap(fmt.Sprintf("Can't remove file %s", path), err)
+	}
+
+	return nil
+}
+
+func (s Storage) IsExists(p *storage.Page) (bool, error) {
+	fileName, err := fileName(p)
+	if err != nil {
+		return false, e.Wrap("Can't remove file", err)
+	}
+
+	path := filepath.Join(s.basePath, p.UserName, fileName)
+
+	switch _, err = os.Stat(path); {
+	case errors.Is(err, os.ErrNotExist):
+		return false, nil
+	case err != nil:
+		return false, e.Wrap(fmt.Sprintf("Can't check if file %s exists", path), err)
+	}
+
+	return true, nil
+}
+
+func (s Storage) decodePage(filePath string) (page *storage.Page, err error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, e.Wrap("Can't decode page", err)
+	}
+
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+
+		}
+	}(file)
+
+	var p storage.Page
+
+	if err := gob.NewDecoder(file).Decode(&p); err != nil {
+		return nil, e.Wrap("Can't decode page", err)
+	}
+
+	return &p, nil
+}
+
+func fileName(p *storage.Page) (string, error) {
+	return p.Hash()
+}
--- storage/storage.go
+++ storage/storage.go
@@ -1,1 +1,35 @@
 package storage
+
+import (
+	"TelegramGOBot/lib/e"
+	"crypto/sha1"
+	"fmt"
+	"io"
+	"time"
+)
+
+type Storage interface {
+	Save(p *Page) error
+	PickRandom(userName string) (p *Page, err error)
+	Remove(p *Page)
+	IsExists(p *Page) (bool, error)
+}
+
+type Page struct {
+	URL      string
+	UserName string
+	Created  time.Time
+}
+
+func (p Page) Hash() (string, error) {
+	h := sha1.New()
+
+	if _, err := io.WriteString(h, p.URL); err != nil {
+		return "", e.Wrap("cat't calculate hash", err)
+	}
+
+	if _, err := io.WriteString(h, p.URL); err != nil {
+		return "", e.Wrap("cat't calculate hash", err)
+	}
+	return fmt.Sprint("%x", h.Sum(nil)), nil
+}
